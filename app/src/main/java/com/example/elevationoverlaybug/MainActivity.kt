33daf0ef55367a57b53fb8565c34f590ccc71b37
Clayton Green <claytron.g@gmail.com>
2022-04-05 17:46:18 -0700

    Show compat mode examples

--- app/src/main/java/com/example/elevationoverlaybug/MainActivity.kt
+++ app/src/main/java/com/example/elevationoverlaybug/MainActivity.kt
@@ -16,11 +16,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         findViewById<TextView>(R.id.white_87).setMaterialShapeDrawable()
+        findViewById<TextView>(R.id.white_87_compat_default).setMaterialShapeDrawable(MaterialShapeDrawable.SHADOW_COMPAT_MODE_DEFAULT)
+        findViewById<TextView>(R.id.white_87_compat_never).setMaterialShapeDrawable(MaterialShapeDrawable.SHADOW_COMPAT_MODE_NEVER)
+        findViewById<TextView>(R.id.white_87_compat_always).setMaterialShapeDrawable(MaterialShapeDrawable.SHADOW_COMPAT_MODE_ALWAYS)
         findViewById<TextView>(R.id.white_87_argb).setMaterialShapeDrawable()
 
         findViewById<Button>(R.id.button).setOnClickListener {
-            val currentNightMode =
-                resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+            val currentNightMode = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
             val mode = when (currentNightMode) {
                 Configuration.UI_MODE_NIGHT_YES -> AppCompatDelegate.MODE_NIGHT_NO
                 Configuration.UI_MODE_NIGHT_NO -> AppCompatDelegate.MODE_NIGHT_YES
@@ -32,12 +34,12 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun View.setMaterialShapeDrawable() {
+    private fun View.setMaterialShapeDrawable(@MaterialShapeDrawable.CompatibilityShadowMode shadowCompatibilityMode: Int? = null) {
         val elevation = resources.getDimension(R.dimen.elevation)
-        val shape = MaterialShapeDrawable.createWithElevationOverlay(
-            context,
-            elevation
-        )
+        val shape = MaterialShapeDrawable.createWithElevationOverlay(context, elevation)
+        if (shadowCompatibilityMode != null) {
+            shape.shadowCompatibilityMode = shadowCompatibilityMode
+        }
         ViewCompat.setBackground(this, shape)
         ViewCompat.setElevation(this, elevation)
     }
